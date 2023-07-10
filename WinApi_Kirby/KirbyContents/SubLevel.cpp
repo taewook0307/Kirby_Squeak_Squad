973b4f8e871487dd06634548b583d12d1d8131da
TaeWook <ktwisland111@gmail.com>
2023-07-10 13:27:43 +0900

    김태욱 : 화면 Effect 사라지지 않는 버그 수정

--- WinApi_Kirby/KirbyContents/SubLevel.cpp
+++ WinApi_Kirby/KirbyContents/SubLevel.cpp
@@ -124,6 +124,11 @@ void SubLevel::FormChange(const std::string& _BitMapFileName)
 		&& LevelPlayer->GetKeepType() == MonsterType::Ice
 		|| true == GameEngineInput::IsDown(VK_F5))
 	{
+		if (nullptr != LevelPlayer->GetChangeBackGroundEffect())
+		{
+			LevelPlayer->GetChangeBackGroundEffect()->Death();
+		}
+
 		GameEngineTime::MainTimer.SetAllTimeScale(0.0f);
 		GameEngineTime::MainTimer.SetTimeScale(UpdateOrder::Player, 1.0f);
 		SetLevelPlayerForm(MonsterType::Ice);
@@ -140,6 +145,11 @@ void SubLevel::FormChange(const std::string& _BitMapFileName)
 		&& LevelPlayer->GetKeepType() == MonsterType::Spark
 		|| true == GameEngineInput::IsDown(VK_F6))
 	{
+		if (nullptr != LevelPlayer->GetChangeBackGroundEffect())
+		{
+			LevelPlayer->GetChangeBackGroundEffect()->Death();
+		}
+
 		GameEngineTime::MainTimer.SetAllTimeScale(0.0f);
 		GameEngineTime::MainTimer.SetTimeScale(UpdateOrder::Player, 1.0f);
 		SetLevelPlayerForm(MonsterType::Spark);
@@ -156,6 +166,11 @@ void SubLevel::FormChange(const std::string& _BitMapFileName)
 		&& LevelPlayer->GetKeepType() == MonsterType::Fire
 		|| true == GameEngineInput::IsDown(VK_F7))
 	{
+		if (nullptr != LevelPlayer->GetChangeBackGroundEffect())
+		{
+			LevelPlayer->GetChangeBackGroundEffect()->Death();
+		}
+
 		GameEngineTime::MainTimer.SetAllTimeScale(0.0f);
 		GameEngineTime::MainTimer.SetTimeScale(UpdateOrder::Player, 1.0f);
 		SetLevelPlayerForm(MonsterType::Fire);
@@ -172,6 +187,11 @@ void SubLevel::FormChange(const std::string& _BitMapFileName)
 		&& LevelPlayer->GetKeepType() == MonsterType::Tornado
 		|| true == GameEngineInput::IsDown(VK_F8))
 	{
+		if (nullptr != LevelPlayer->GetChangeBackGroundEffect())
+		{
+			LevelPlayer->GetChangeBackGroundEffect()->Death();
+		}
+
 		GameEngineTime::MainTimer.SetAllTimeScale(0.0f);
 		GameEngineTime::MainTimer.SetTimeScale(UpdateOrder::Player, 1.0f);
 		SetLevelPlayerForm(MonsterType::Tornado);
